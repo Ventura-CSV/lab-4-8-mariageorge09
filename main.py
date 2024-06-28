92d4c1797636635f0423b0fdf8e196322020df63
Maria <emailtomariageorge@gmail.com>
2024-06-27 17:00:18 -0700

    4-8

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def main():
     plist = []
+    begin = int(input('Enter first number: '))
+    end = int(input('Enter second number: '))
+    for num in range(begin, end +1 , 1):
+         if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                plist.append(num)
+    print('Output:  ')
+    print(plist)
     ##################################################
     # Comlete your code here
     ##################################################
